b189ac50ab16e7d69a98049219a454641daa2e41
yeeun_An <an446@naver.com>
2022-05-28 18:07:14 +0900

    :sparkles: Chart component apexcharts

--- crypto-tracker/src/api.tsx
+++ crypto-tracker/src/api.tsx
@@ -15,3 +15,12 @@ export function fetchCoinTickers(coinId: string | undefined) {
     response.json()
   );
 }
+
+//암호화폐의 highest(가장 올랐을 때), loweat, close, volum을 array로 가지고 있는 데이터
+export function fetchCoinHistory(coinId: string | undefined) {
+  const endDate = Math.floor(Date.now() / 1000); //이거는 fetch할때 필수로 들어가야하는 props야 api 명세서에 있음
+  const startDate = endDate - 60 * 60 * 24 * 7; //endDate으로부터 일주일 전
+  return fetch(
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+  ).then((response) => response.json());
+}
--- crypto-tracker/src/routes/Chart.tsx
+++ crypto-tracker/src/routes/Chart.tsx
@@ -1,10 +1,86 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useOutletContext } from "react-router";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ReactApexChart from "react-apexcharts";
 
+interface ChartProps {
+  coinId: string;
+}
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 export default function Chart() {
+  //1.useParams사용해서 coinId가져오기
+  const params = useParams();
+  //2.Outlet에서 가져오기
+  const coinId = useOutletContext() as ChartProps["coinId"];
+
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
   return (
     <>
-      <h1>Chart</h1>
-      <p> hello</p>
+      <div>
+        {/* series라는 porps에 보낼 데이터를(차트에 보여줄) 넣어줌 */}
+        {isLoading ? (
+          "Loading chart..."
+        ) : (
+          <ReactApexChart
+            type="line"
+            // series가 props로 받아야하는 건 number임. 그렇지만 data를 못받으면 undefined가 되기 때문에 number임을 강제로 알려야함. 그래서 as쓴 거임.
+            series={[
+              {
+                name: "Price",
+                data: data?.map((price) => price.close) as number[],
+              },
+            ]}
+            options={{
+              chart: {
+                height: 500,
+                width: 500,
+                toolbar: {
+                  show: false,
+                },
+                background: "transparent",
+              },
+              grid: {
+                show: false,
+              },
+              yaxis: {
+                show: false,
+              },
+              xaxis: {
+                axisTicks: { show: false },
+                labels: {
+                  show: false,
+                },
+              },
+              theme: {
+                mode: "dark",
+                palette: "palette5",
+                monochrome: {
+                  enabled: false,
+                  color: "#255aee",
+                  shadeTo: "light",
+                  shadeIntensity: 0.65,
+                },
+              },
+              stroke: {
+                curve: "smooth",
+                width: 3,
+              },
+            }}
+          />
+        )}
+      </div>
     </>
   );
 }
