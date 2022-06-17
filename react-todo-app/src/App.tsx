2716f53455fc56b37d637be3e88e13426eabaf84
yeeun_An <an446@naver.com>
2022-06-18 03:43:05 +0900

    :sparkles: useMoionValue, useTransform -> box 이동시 Wrapper bg변화

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -1,15 +1,17 @@
 // import ToDoList from "./ToDoList";
 
 import styled from "styled-components";
-
-import { motion } from "framer-motion";
+import { useEffect } from "react";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useRef } from "react";
-const Wrapper = styled.div`
+
+const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, #f97794, #623aa2);
 `;
 
 //Variants
@@ -98,12 +100,33 @@ const BiggerBox = styled.div`
   overflow: hidden;
 `;
 
+//MotionValue
+const Box_M = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 30px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function App() {
   const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 0, 800],
+    [
+      "linear-gradient(135deg, rgb(247, 220, 111), rgb(249, 235, 234))",
+      "linear-gradient(135deg, rgb(217, 136, 128), rgb(136, 78, 160))",
+      "linear-gradient(135deg, rgb(205, 97, 85), rgb(93, 173, 226))",
+    ]
+  );
+
   return (
     <>
-      <Wrapper>
-        <Box variants={boxVariants} initial="start" animate="end">
+      <Wrapper style={{ background: gradient }}>
+        {/* <Box variants={boxVariants} initial="start" animate="end">
           <Circle variants={circleVariants} />
           <Circle variants={circleVariants} />
           <Circle variants={circleVariants} />
@@ -121,7 +144,10 @@ function App() {
             whileTap="click"
             whileDrag="drag"
           />
-        </BiggerBox>
+        </BiggerBox> */}
+
+        {/* <button onClick={() => x.set(200)}>click me</button> */}
+        <Box_M style={{ x, rotateZ }} drag="x" dragSnapToOrigin />
       </Wrapper>
     </>
   );
